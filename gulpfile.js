4558d7eb0d724ad2282d79b91e34a8ca02a43629
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-01-19 16:20:45 -0500

    Working on getting gulp to spin up a server

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,47 @@
+
+var del = require('del');
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json', {typescript: require('typescript')});
-
-
+var connect = require('gulp-connect');
 
 gulp.task('js', function() {
     var tsResult = tsProject.src()     
       .pipe(ts(tsProject));
     
     return tsResult.js
+      .pipe(rename(function (path) {
+        path.dirname = path.dirname.replace('src', '');
+      }))
       .pipe(gulp.dest('dist/'));
 });
 
-
-
 gulp.task('html', function () {
     return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist/src'));
+        .pipe(gulp.dest('dist'));
 });
-
 
 gulp.task('clean', function(done) {
   del(['dist'], done);
 });
 
-
+gulp.task('server', function() {
+  connect.server({
+    root: 'dist/',
+    port: 8889
+  });
+});
 
 gulp.task('libs', function () {
     return gulp.src([
-          'bundle/angular2.dev.js',
-          'node_modules/traceur/bin/traceur-runtime.js',
-          'node_modules/es6-module-loader/dist/es6-module-loader.js',
-          'node_modules/systemjs/dist/system.js'          
+          'node_modules/angular2/bundles/angular2-polyfills.js',
+          'node_modules/systemjs/dist/system.src.js',
+          'node_modules/rxjs/bundles/Rx.js',
+          'node_modules/angular2/bundles/angular2.dev.js',
+          'node_modules/angular2/bundles/router.dev.js'   
         ])
       .pipe(gulp.dest('dist/lib'));
 });
 
-gulp.task('default', ['js', 'libs', 'html']);
+gulp.task('default', ['js', 'libs', 'html', 'server']);
