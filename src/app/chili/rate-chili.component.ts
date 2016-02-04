36e5ad733c972c5cecc846d31b602461318ebb36
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-02-04 16:25:39 -0500

    Ensuring that the rating input does not modify the rating state

--- src/app/chili/rate-chili.component.ts
+++ src/app/chili/rate-chili.component.ts
@@ -9,66 +9,83 @@ import {RateInputComponent} from '../rating/rate-input.component';
 import {RatingActions} from '../rating/rating.actions';
 
 @Component({
-  selector: 'vote',
-  templateUrl: 'app/chili/rate-chili.component.html',
-  styleUrls: ['app/chili/rate-chili.component.css'],
-  directives: [RateInputComponent],
-  providers: [RatingActions]
+    selector: 'vote',
+    templateUrl: 'app/chili/rate-chili.component.html',
+    styleUrls: ['app/chili/rate-chili.component.css'],
+    directives: [RateInputComponent],
+    providers: [RatingActions]
 })
 export class RatingComponent implements OnInit {
-  public chili: Chili;
-  public rating: Rating;
-  public currentJudge: Judge;
+    public chili: Chili;
+    public rating: Rating;
+    public currentJudge: Judge;
+    public ratingEntriesCopy = [];
 
-  constructor(
-    @Inject('AppStore') private _appStore: AppStore,
-    private _chiliService: ChiliService,
-    private _ratingService: RatingService,
-    private _ratingActions: RatingActions,
-    private _routeParams: RouteParams, private _routeData: RouteData) {
-  }
-
-  ngOnInit() {
-    this.currentJudge = this._appStore.getState().judges.currentJudge;
-
-    if (!this.chili) {
-      let id = +this._routeParams.get('id');
-      this._chiliService.getChili(id).then(chili => {
-        this.chili = chili;
-        var ratingList = this._appStore.getState().rating.ratingList;
-        var foundRating = ratingList.find(rating =>
-          rating.chili.id === this.chili.id && rating.judge.id === this.currentJudge.id
-        );
-
-        if (foundRating === undefined) {
-          this._ratingService.createRatingSetForChili(this.chili, this.currentJudge).then(rating =>
-            this.rating = rating
-          );
-        } else {
-          this.rating = foundRating;
-        }
-      });
+    constructor(
+        @Inject('AppStore') private _appStore: AppStore,
+        private _chiliService: ChiliService,
+        private _ratingService: RatingService,
+        private _ratingActions: RatingActions,
+        private _routeParams: RouteParams, private _routeData: RouteData) {
     }
-  }
 
-  resetRating() {
-    this.rating.ratingEntries.forEach(ratingEntry => ratingEntry.ratingValue = null);
-  }
+    ngOnInit() {
+        this.currentJudge = this._appStore.getState().judges.currentJudge;
 
-  submitRating() {
-    this._appStore.dispatch(this._ratingActions.addRating(this.rating));
-    console.log(this._appStore.getState().rating);
-  }
+        let chiliId = +this._routeParams.get('id');
+        this._chiliService.getChili(chiliId).then(chili => {
+            this.chili = chili;
+            var ratingList = this._appStore.getState().rating.ratingList;
+            var foundRating = ratingList.find(rating =>
+                rating.chili.id === this.chili.id && rating.judge.id === this.currentJudge.id
+                );
 
-  allRatingsFilled() {
-    var ratingsFilled: number = 0;
-    this.rating.ratingEntries.forEach(ratingEntry => {
-      if (ratingEntry.ratingValue !== null) {
-        ratingsFilled++;
-      }
-    });
+            if (foundRating === undefined) {
+                this._ratingService.createRatingSetForChili(this.chili, this.currentJudge).then(rating => {
+                    this.rating = rating;
+                    this.ratingEntriesCopy = this.createRatingEntriesCopy(this.rating.ratingEntries);
+                });
+            } else {
+                this.rating = foundRating;
+                this.ratingEntriesCopy = this.createRatingEntriesCopy(this.rating.ratingEntries);
+            }
+        });
+    }
 
-    return ratingsFilled === this.rating.ratingEntries.length;
-  }
+    private createRatingEntriesCopy(ratingEntries) {
+        var ratingEntriesCopy = []
+        ratingEntries.forEach(ratingValue => {
+            ratingEntriesCopy.push({
+                ratingValue: ratingValue.ratingValue,
+                category: ratingValue.category
+            });
+        });
+
+        return ratingEntriesCopy;
+    }
+
+    resetRating() {
+        this.ratingEntriesCopy.forEach(ratingEntry => ratingEntry.ratingValue = null);
+    }
+
+    submitRating() {
+        this._appStore.dispatch(this._ratingActions.addRating(
+            Object.assign({}, this.rating, {
+                ratingEntries: this.ratingEntriesCopy
+            })
+            ));
+        console.log(this._appStore.getState().rating);
+    }
+
+    allRatingsFilled() {
+        var ratingsFilled: number = 0;
+        this.ratingEntriesCopy.forEach(ratingEntry => {
+            if (ratingEntry.ratingValue !== null) {
+                ratingsFilled++;
+            }
+        });
+
+        return ratingsFilled === this.rating.ratingEntries.length;
+    }
 }
 
